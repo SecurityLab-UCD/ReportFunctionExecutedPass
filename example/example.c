23561ab9f05dc08dd5dc2861c4368bbf4675c161
Yifeng He <yfhe@ucdavis.edu>
2023-01-30 15:18:58 -0800

    modify example to instrument lib object

--- example/example.c
+++ example/example.c
@@ -10,12 +10,19 @@
 
 #include "lib.h"
 
+int add(int x, int y) { return x + y; }
+int add1(int x) { return add(x, 1); }
+
 int main() {
   int j, n, m, sum;
   n = 5;
+  m = add1(add1(n));
+  printf("abc\n");
+  printf("abc\n");
   for (j = 1; j <= n; j++) {
     m = 3;
     sum = zy(2, m);
     printf("%d\n", sum);
   }
+  return 0;
 }
