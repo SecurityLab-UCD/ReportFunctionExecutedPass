c6d0de019204f8103aa5da62d3d50ff817f3f3da
Ethan He <yfhe@ucdavis.edu>
2023-03-27 03:27:06 +0000

    fix: recursive struct expand use getPointerOperand()

--- example.c
+++ example.c
@@ -25,9 +25,38 @@ double foo(struct LargeStruct s) {
   return e + f;
 }
 
+struct LinkedNode {
+  int value;
+  struct LinkedNode *next;
+};
+
+struct LinkedNode *create_list(int n) {
+  struct LinkedNode *head = NULL;
+  for (int i = 0; i < n; i++) {
+    struct LinkedNode *node =
+        (struct LinkedNode *)malloc(sizeof(struct LinkedNode));
+    node->value = i;
+    node->next = head;
+    head = node;
+  }
+  return head;
+}
+
+void print_list(struct LinkedNode *head) {
+  struct LinkedNode *current = head;
+  while (current != NULL) {
+    printf("%d ", current->value);
+    current = current->next;
+  }
+  putchar('\n');
+}
+
 int main() {
-  struct LargeStruct s = {1, 2, 3.0, 4.0, 5.0, 6.0};
-  double result = foo(s);
-  printf("result = %f\n", result);
+  // struct LargeStruct s = {1, 2, 3.0, 4.0, 5.0, 6.0};
+  // double result = foo(s);
+  // printf("result = %f\n", result);
+
+  struct LinkedNode *head = create_list(3);
+  print_list(head);
   return 0;
 }
