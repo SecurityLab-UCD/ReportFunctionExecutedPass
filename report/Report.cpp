53a143fbc5f91a8d0dd92618e3fd6fac6ba3edd1
Ethan He <yfhe@ucdavis.edu>
2023-04-09 20:03:20 +0000

    feat: split report to two calls at entry and exit

--- report/Report.cpp
+++ report/Report.cpp
@@ -128,10 +128,113 @@ bool is_in(std::string str, std::vector<std::string> &vec) {
   return std::find(vec.begin(), vec.end(), str) != vec.end();
 }
 
+std::vector<Value *> ReportInputs(Function &F, FunctionCallee &ReportParam,
+                                  raw_string_ostream &rso,
+                                  std::string delimiter) {
+  Instruction *EntryInst = &*F.getEntryBlock().begin();
+  LLVMContext &Ctx = F.getContext();
+  Module *M = F.getParent();
+
+  std::vector<Value *> InputArgs;
+  std::vector<Value *> PointerArgs;
+  for (Value &Arg : F.args()) {
+    if (isStructPtrTy(Arg.getType())) {
+      std::vector<Value *> elems = ExpandStruct(&Arg, &F, EntryInst);
+      InputArgs.insert(InputArgs.end(), elems.begin(), elems.end());
+      PointerArgs.insert(PointerArgs.end(), elems.begin(), elems.end());
+
+      // todo: get this type string from ExpandStruct
+      rso << GetTyStr(elems, delimiter);
+    } else {
+      InputArgs.push_back(&Arg);
+      Arg.getType()->print(rso);
+
+      if (Arg.getType()->isPointerTy()) {
+        PointerArgs.push_back(&Arg);
+      }
+    }
+    rso << delimiter;
+  }
+
+  APInt InputArgsLen = APInt(32, InputArgs.size(), false);
+  InputArgs.insert(InputArgs.begin(), ConstantInt::get(Ctx, InputArgsLen));
+  InputArgs.insert(InputArgs.begin(), MakeGlobalString(M, rso.str()));
+  APInt IsRnt = APInt(1, false, false);
+  InputArgs.insert(InputArgs.begin(), ConstantInt::get(Ctx, IsRnt));
+  CallInst::Create(ReportParam, InputArgs, "report_param", EntryInst);
+  return PointerArgs;
+}
+
+void ReportOutputs(Function &F, FunctionCallee &ReportParam,
+                   std::vector<Value *> &PrevPointerInputs,
+                   raw_string_ostream &rso, std::string delimiter) {
+  std::vector<Value *> RetVals;
+  LLVMContext &Ctx = F.getContext();
+  Module *M = F.getParent();
+
+  // find rnt value and terminating instruction
+  Instruction *ReportInsertB4 = nullptr;
+  for (BasicBlock &BB : F) {
+    Instruction *Term = BB.getTerminator();
+
+    // match on different type of terminator
+    // empty else-if branches are reserved for later changes
+    if (ReturnInst *RI = dyn_cast<ReturnInst>(Term)) {
+      ReportInsertB4 = RI;
+      if (RI->getNumOperands() == 1) {
+        Value *ReturnValue = RI->getOperand(0);
+        if (isStructPtrTy(ReturnValue->getType())) {
+          std::vector<Value *> elems = ExpandStruct(ReturnValue, &F, RI);
+          RetVals.insert(RetVals.end(), elems.begin(), elems.end());
+
+          // todo: get this type string from ExpandStruct
+          rso << GetTyStr(elems, delimiter);
+        } else {
+          RetVals.push_back(ReturnValue);
+          ReturnValue->getType()->print(rso);
+          rso << delimiter;
+        }
+      }
+    } else if (SwitchInst *SI = dyn_cast<SwitchInst>(Term)) {
+    } else if (BranchInst *BI = dyn_cast<BranchInst>(Term)) {
+    } else if (IndirectBrInst *IBI = dyn_cast<IndirectBrInst>(Term)) {
+    } else if (CallBrInst *CBI = dyn_cast<CallBrInst>(Term)) {
+    } else if (InvokeInst *II = dyn_cast<InvokeInst>(Term)) {
+    } else if (ResumeInst *RI = dyn_cast<ResumeInst>(Term)) {
+    } else if (CatchSwitchInst *CSI = dyn_cast<CatchSwitchInst>(Term)) {
+    } else if (CatchReturnInst *CRI = dyn_cast<CatchReturnInst>(Term)) {
+    } else if (CleanupReturnInst *CuRI = dyn_cast<CleanupReturnInst>(Term)) {
+    } else if (UnreachableInst *UI = dyn_cast<UnreachableInst>(Term)) {
+      errs() << F.getName() << " Found Unreachable Terminator:\n\t";
+      UI->print(errs());
+      errs() << "\n";
+    } else {
+      errs() << "Unknown Terminator:\n\t";
+      Term->print(errs());
+    }
+  }
+
+  if (ReportInsertB4) {
+    // reinsert pointer inputs
+    for (Value *PointerInput : PrevPointerInputs) {
+      RetVals.push_back(PointerInput);
+      PointerInput->getType()->print(rso);
+      rso << delimiter;
+    }
+    APInt RetValsLen = APInt(32, RetVals.size(), false);
+    RetVals.insert(RetVals.begin(), ConstantInt::get(Ctx, RetValsLen));
+    RetVals.insert(RetVals.begin(), MakeGlobalString(M, rso.str()));
+    APInt IsRnt = APInt(1, true, false);
+    RetVals.insert(RetVals.begin(), ConstantInt::get(Ctx, IsRnt));
+    CallInst::Create(ReportParam, RetVals, "report_param", ReportInsertB4);
+  }
+}
+
 bool ReportPass::runOnFunction(Function &F) {
   std::string fname = F.getName().str();
   Module *M = F.getParent();
   BasicBlock &entry = F.getEntryBlock();
+  Instruction *EntryInst = &*entry.begin();
   LLVMContext &Ctx = F.getContext();
   std::string file_name = F.getParent()->getSourceFileName();
   std::string file_name_wo_ext =
@@ -161,7 +264,7 @@ bool ReportPass::runOnFunction(Function &F) {
     std::vector<Value *> AtexitArgs({dump});
     FunctionCallee DumpAtexit = M->getOrInsertFunction("atexit", AtexitFTy);
     Instruction *AtexitInst =
-        CallInst::Create(DumpAtexit, AtexitArgs, "atexit", &*entry.begin());
+        CallInst::Create(DumpAtexit, AtexitArgs, "atexit", EntryInst);
 
   } else {
 
@@ -182,80 +285,19 @@ bool ReportPass::runOnFunction(Function &F) {
     // the middle tokens are parameter types
     // the last token is return type
     // func_name>>=param1_type>>=...>>=rnt_type>>=
-    std::string TypeStr = file_name_wo_ext + "_" + fname + delimiter;
-    std::vector<Value *> ParamArgs;
-    llvm::raw_string_ostream rso(TypeStr);
-    for (Value &Arg : F.args()) {
-      if (isStructPtrTy(Arg.getType())) {
-        std::vector<Value *> elems = ExpandStruct(&Arg, &F, &*entry.begin());
-        ParamArgs.insert(ParamArgs.end(), elems.begin(), elems.end());
+    std::string TypeStrStarter = file_name_wo_ext + "_" + fname + delimiter;
 
-        // todo: get this type string from ExpandStruct
-        rso << GetTyStr(elems, delimiter);
-      } else {
-        ParamArgs.push_back(&Arg);
-        Arg.getType()->print(rso);
-      }
-      rso << delimiter;
-    }
-    APInt ParamArgsLen = APInt(32, ParamArgs.size(), false);
-    ParamArgs.insert(ParamArgs.begin(), ConstantInt::get(Ctx, ParamArgsLen));
+    // insert call to report at entry with input parameters
+    std::string InputsTyStr = TypeStrStarter;
+    raw_string_ostream irso(InputsTyStr);
+    std::vector<Value *> PrevPointerInputs =
+        ReportInputs(F, ReportParam, irso, delimiter);
 
-    // find rnt value and terminating instruction
-    // todo: expand return struct type
-    Instruction *ReportInsertB4 = &*entry.begin();
-    bool has_rnt = false;
-    for (BasicBlock &BB : F) {
-      Instruction *Term = BB.getTerminator();
-
-      // match on different type of terminator
-      // empty else-if branches are reserved for later changes
-      if (ReturnInst *RI = dyn_cast<ReturnInst>(Term)) {
-        ReportInsertB4 = RI;
-        if (RI->getNumOperands() == 1) {
-          Value *ReturnValue = RI->getOperand(0);
-          if (isStructPtrTy(ReturnValue->getType())) {
-            std::vector<Value *> elems = ExpandStruct(ReturnValue, &F, RI);
-            ParamArgs.insert(ParamArgs.end(), elems.begin(), elems.end());
-
-            // todo: get this type string from ExpandStruct
-            rso << GetTyStr(elems, delimiter);
-          } else {
-            ParamArgs.push_back(ReturnValue);
-          }
-          has_rnt = true;
-        }
-      } else if (SwitchInst *SI = dyn_cast<SwitchInst>(Term)) {
-      } else if (BranchInst *BI = dyn_cast<BranchInst>(Term)) {
-      } else if (IndirectBrInst *IBI = dyn_cast<IndirectBrInst>(Term)) {
-      } else if (CallBrInst *CBI = dyn_cast<CallBrInst>(Term)) {
-      } else if (InvokeInst *II = dyn_cast<InvokeInst>(Term)) {
-      } else if (ResumeInst *RI = dyn_cast<ResumeInst>(Term)) {
-      } else if (CatchSwitchInst *CSI = dyn_cast<CatchSwitchInst>(Term)) {
-      } else if (CatchReturnInst *CRI = dyn_cast<CatchReturnInst>(Term)) {
-      } else if (CleanupReturnInst *CuRI = dyn_cast<CleanupReturnInst>(Term)) {
-      } else if (UnreachableInst *UI = dyn_cast<UnreachableInst>(Term)) {
-        errs() << F.getName() << " Found Unreachable Terminator:\n\t";
-        UI->print(errs());
-        errs() << "\n";
-      } else {
-        errs() << "Unknown Terminator:\n\t";
-        Term->print(errs());
-      }
-    }
-
-    // add rnt type to the end of type string
-    if (has_rnt) {
-      F.getReturnType()->print(rso);
-      rso << delimiter;
-    }
-
-    ParamArgs.insert(ParamArgs.begin(), MakeGlobalString(M, TypeStr));
-
-    APInt HasRnt = APInt(1, has_rnt, false);
-    ParamArgs.insert(ParamArgs.begin(), ConstantInt::get(Ctx, HasRnt));
-
-    CallInst::Create(ReportParam, ParamArgs, "report_param", ReportInsertB4);
+    // todo: insert call to report at exit with return values
+    // and pointer inputs
+    std::string OutputsTyStr = TypeStrStarter;
+    raw_string_ostream orso(OutputsTyStr);
+    ReportOutputs(F, ReportParam, PrevPointerInputs, orso, delimiter);
   }
   return true;
 }
